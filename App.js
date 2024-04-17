b6605d50670359c8fd16ee31767625a78c7af502
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-04-18 01:45:09 +0200

    Starting the build for controlled form.

---
+++ App.js
@@ -0,0 +1,9 @@
+class ControlledForm extends React.Component {
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
+
+ReactDOM.render(<ControlledForm />, document.getElementById("root"));
