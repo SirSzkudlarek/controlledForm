ce89099546b695600667dd1b796647e39af2da24
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-04-21 20:16:32 +0200

    Created controlled form with usage of state.

--- App.js
+++ App.js
@@ -1,5 +1,58 @@
 class ControlledForm extends React.Component {
-  state = {};
+  state = {
+    firstName: "",
+    secondName: "",
+    password: "",
+    rePassword: "",
+    email: "",
+    phoneNumber: "",
+    checkbox: false,
+  };
+
+  handleChange = (e) => {
+    const selectedField = e.target.id;
+    if (e.target.type === "checkbox") {
+      this.setState({
+        [selectedField]: e.target.checked,
+      });
+    } else {
+      this.setState({
+        [selectedField]: e.target.value,
+      });
+    }
+  };
+
+  handleSubmit = () => {
+    const props = this.state;
+    const numberRegex = /^[0-9]+$/;
+    if (props.password !== props.rePassword) {
+      alert("Password fields doesn't match!");
+    } else if (props.password.length < 6) {
+      alert(
+        "Your password is to short! Minimal length should contains at least 6 marks."
+      );
+    } else if (props.checkbox !== true) {
+      alert("You have to accept our policy privacy!");
+    } else if (props.phoneNumber.length < 9) {
+      alert("Phone number is to short.");
+    } else if (!props.email.includes("@")) {
+      alert("Provieded adres email is wrong.");
+    } else if (!numberRegex.test(props.phoneNumber)) {
+      alert("Phone numbers only contains digits.");
+    } else if (
+      !(
+        props.firstName !== "" &&
+        props.secondName !== "" &&
+        props.password !== "" &&
+        props.rePassword !== "" &&
+        props.email !== ""
+      )
+    ) {
+      alert("You have left empty fields.");
+    } else {
+      console.log(props);
+    }
+  };
 
   render() {
     return (
@@ -9,28 +62,80 @@ class ControlledForm extends React.Component {
         <form class="form">
           <div class="line">
             <div class="column">
-              <label for="first-name">First Name</label>
-              <input type="text" name="first-name"></input>
+              <label class="label" for="first-name">
+                First Name
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                name="first-name"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              ></input>
             </div>
             <div class="column">
-              <label for="second-name">Second Name</label>
-              <input type="text" name="second-name"></input>
+              <label class="label" for="second-name">
+                Second Name
+              </label>
+              <input
+                type="text"
+                id="secondName"
+                name="second-name"
+                value={this.state.secondName}
+                onChange={this.handleChange}
+              ></input>
             </div>
           </div>
           <div class="line">
             <div class="column">
-              <label for="pass">Password</label>
-              <input type="password" name="pass"></input>
+              <label class="label" for="pass">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="pass"
+                value={this.state.password}
+                onChange={this.handleChange}
+              ></input>
             </div>
             <div class="column">
-              <label for="re-pass">Repeat password</label>
-              <input type="password" name="re-pass"></input>
+              <label class="label" for="re-pass">
+                Repeat password
+              </label>
+              <input
+                type="password"
+                id="rePassword"
+                name="re-pass"
+                value={this.state.rePassword}
+                onChange={this.handleChange}
+              ></input>
             </div>
           </div>
           <div class="line">
             <div class="column">
-              <label for="email">Email</label>
-              <input type="email" name="email"></input>
+              <label class="label" for="email">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              ></input>
+            </div>
+            <div class="column">
+              <label class="label" for="phone-number">
+                Phone number
+              </label>
+              <input
+                type="tel"
+                id="phoneNumber"
+                name="phone-number"
+                value={this.state.phoneNumber}
+                onChange={this.handleChange}
+              ></input>
             </div>
           </div>
           <div class="line">
@@ -39,14 +144,24 @@ class ControlledForm extends React.Component {
                 Do you accept our law and policy of this website?
               </h2>
               <div class="checkbox-wrapper">
-                <label for="checkbox">Yes, I accept!</label>
-                <input type="checkbox" name="checkbox"></input>
+                <label class="label" for="checkbox">
+                  Yes, I accept!
+                </label>
+                <input
+                  type="checkbox"
+                  id="checkbox"
+                  name="checkbox"
+                  checked={this.state.checkbox}
+                  onChange={this.handleChange}
+                ></input>
               </div>
             </div>
           </div>
 
           <div class="line">
-            <button class="button">Send!</button>
+            <button class="button" type="button" onClick={this.handleSubmit}>
+              Send!
+            </button>
           </div>
         </form>
       </div>
